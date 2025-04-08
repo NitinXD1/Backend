573fc7f39508f8b9c9be576464f321e1353885f0
NitinXD1 <nitinkachera98@gmail.com>
2025-04-08 18:20:03 +0530

    simple express setup app

--- Basics/index.js
+++ Basics/index.js
@@ -1,1 +1,20 @@
-console.log('Hello, world! This is a test.');
+const express = require('express')
+require('dotenv').config()
+//app is powerful like MATh
+
+//sensitive info is passed from .env file
+const port = 4000
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter',(req,res) => {
+    res.send(`<h1>Twitter is here</h1>`)
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`)
+})
