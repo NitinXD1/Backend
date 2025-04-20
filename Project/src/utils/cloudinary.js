e8e48eda14f0622b7c30382cda8f7f31c873d58a
NitinXD1 <nitinkachera98@gmail.com>
2025-04-20 17:04:19 +0530

    cloudinary and multer

---
+++ Project/src/utils/cloudinary.js
@@ -0,0 +1,31 @@
+//getting the file from local storage and sending it to cloudinary
+
+import {v2 as cloudinary} from 'cloudinary'
+import fs from 'fs'
+
+cloudinary.config({
+    cloud_name : process.env.CLOUDINARY_CLOUD_NAME,
+    api_key : process.env.CLOUDINARY_API_KEY, 
+    api_secret : process.env.CLOUDINARY_API_SECRET
+})
+
+const uploadOnCloudinary = (localFilePath) =>{
+    try{
+        if(!localFilePath)return null
+        
+        //this returns an object which contains the whole data about the 
+        //file such as its url,size,pixels,etc
+        const res = cloudinary.uploader.upload(localFilePath,{
+            resource_type : auto
+        })
+        console.log("file is successfully uploaded on the server with stats:",res)
+        return res
+    }
+    catch(err){
+        fs.unlinkSync(localFilePath)
+        //file wasnt properly in the local storage remove it and reupload it on local storage
+        return null
+    }
+}
+
+export {uploadOnCloudinary}
